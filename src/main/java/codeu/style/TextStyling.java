6d32f8dce94a59d21ddae372494326eedd616bf3
Talia Bahar <taliabahar@Talias-MacBook-Pro-2.local>
2018-07-12 12:10:29 -0700

    added emojis and notification tab

--- src/main/java/codeu/style/TextStyling.java
+++ src/main/java/codeu/style/TextStyling.java
@@ -4,7 +4,10 @@ import codeu.model.store.basic.UserStore;
 import org.kefirsf.bb.BBProcessorFactory;
 import org.kefirsf.bb.TextProcessor;
 import emoji4j.EmojiUtils;
-import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TextStyling {
   /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -27,43 +30,4 @@ public class TextStyling {
      String emojified = EmojiUtils.emojify(text);
      return emojified;
    }
-
-  // regular expression to parse from string to space into an array than go through the array and see if it is a valid user
-  // split text by the regular expression = an array of strings
-  // go through array and check if it is valid user
-  // substring to remove @
-  // .replace
-
-    public static String styleUserTags2(String text) {
-        // putting tagged user names in an array
-        String[] taggedUsers = text.split("@[A-Za-z0-9]*");
-        String[] userNames = new String[1];
-        String[] validTaggedUsers = new String[1];
-        int count = 0;
-        for (String user : taggedUsers) {
-          userNames[count] = user.substring(1,user.length());
-          count++;
-        }
-        int count2 = 0;
-        for (String userName : userNames) {
-          if(UserStore.getInstance().getUser(userName) != null) {
-              validTaggedUsers[count2] = userName;
-            }
-          }
-        String editedText = "";
-        for (String users : validTaggedUsers) {
-          editedText = text.replace(users, "<span id='userMentioned'>" + "@" + users + "</span>");
-        }
-        return editedText;
-      }
-
-   public static void main(String[] args) {
-      System.out.println("here");
-     TextSyle x = new TextStyle();
-     System.out.println(x.styleUserTags2(“@j”));
-    //  System.out.println(styleUserTags2("hi @Talia"));
-    //  System.out.println(styleUserTags2("@j"));
-    //  System.out.println(styleUserTags("@Talia"));
-    //  System.out.println(styleUserTags("@Patrick"));
-  }
 }
