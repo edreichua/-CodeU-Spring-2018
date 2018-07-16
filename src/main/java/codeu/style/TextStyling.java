67dbe3285d7c00557fa18a24fbe38c2011a16ce3
Talia Bahar <taliabahar@Talias-MacBook-Pro-2.local>
2018-07-16 11:08:42 -0700

    changes

--- src/main/java/codeu/style/TextStyling.java
+++ src/main/java/codeu/style/TextStyling.java
@@ -30,4 +30,56 @@ public class TextStyling {
      String emojified = EmojiUtils.emojify(text);
      return emojified;
    }
+
+
+   /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    *  Method to style tagged users in chat differently
+    *  @param  text         String representing user's original message
+    *  @return String with a span wrapped around tagged usernames to assign it special styling properties
+    *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+   public static String styleUserTags2(String text) {
+        List<String> taggedUsers = new ArrayList<>();
+        List<String> userNames = new ArrayList<>();
+        List<String> validTaggedUsers = new ArrayList<>();
+        String pattern = "@[A-Za-z0-9]*";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(text);
+        while (m.find( )) {
+          taggedUsers.add(m.group(0) );
+        }
+        for (String user : taggedUsers) {
+          userNames.add(user.substring(1,user.length()));
+        }
+        for (String userName : userNames) {
+          System.out.println("USERNAMES USED: " + userName);
+          System.out.println("VALID OR NULL: " + UserStore.getInstance().getUser(userName));
+          if(UserStore.getInstance().isUserRegistered(userName)) {
+              validTaggedUsers.add(userName);
+          }
+          System.out.println("VALID TAGGED USERS" + validTaggedUsers);
+        }
+        String editedText = text;
+        for (String user : validTaggedUsers) {
+          String userWithTag = "@" + user;
+          editedText = editedText.replace(userWithTag, "<span class='userMentioned'>" + userWithTag + "</span>");
+        }
+        return editedText;
+    }
+
+
+// TESTING FOR USERS
+     public static String styleUserTags(String text){
+     if(UserStore.getInstance().isUserRegistered(text)) {
+       return "WORKS";
+     } else {
+       return "DOESN'T WORK";
+     }
+   }
+
+
+   public static void main(String[] args) {
+     System.out.println("here");
+     System.out.println(styleUserTags2("hi @Talia, @Edrei and @Kevin! How are you?"));
+     System.out.println(styleUserTags("Talia"));
+  }
 }
