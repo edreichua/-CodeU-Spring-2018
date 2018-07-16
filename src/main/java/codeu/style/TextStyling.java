a160128d8ea13567a9e725e245de619e698043e5
Talia Bahar <taliabahar@Talias-MacBook-Pro-2.local>
2018-07-16 10:47:36 -0700

    changed ID into class

--- src/main/java/codeu/style/TextStyling.java
+++ src/main/java/codeu/style/TextStyling.java
@@ -1,13 +1,13 @@
 package codeu.style;
 
 import codeu.model.store.basic.UserStore;
-import org.kefirsf.bb.BBProcessorFactory;
-import org.kefirsf.bb.TextProcessor;
 import emoji4j.EmojiUtils;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.kefirsf.bb.BBProcessorFactory;
+import org.kefirsf.bb.TextProcessor;
 
 public class TextStyling {
   /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -30,4 +30,56 @@ public class TextStyling {
      String emojified = EmojiUtils.emojify(text);
      return emojified;
    }
+
+
+   /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    *  Method to style tagged users in chat differently
+    *  @param  text         String representing user's original message
+    *  @return String with a span wrapped around tagged usernames to assign it special styling properties
+    *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+   public static String styleUserTags2(String text) {
+        List<String> taggedUsers = new ArrayList<>();
+        List<String> userNames = new ArrayList<>();
+        List<String> validTaggedUsers = new ArrayList<>();
+        String pattern = "@[A-Za-z0-9]*";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(text);
+        while (m.find( )) {
+          taggedUsers.add(m.group(0) );
+        }
+        for (String user : taggedUsers) {
+          userNames.add(user.substring(1,user.length()));
+        }
+        for (String userName : userNames) {
+          System.out.println("USERNAMES USED: " + userName);
+          System.out.println("VALID OR NULL: " + UserStore.getInstance().getUser(userName));
+          if(UserStore.getInstance().getUser(userName) != null) {
+              validTaggedUsers.add(userName);
+          }
+          System.out.println("VALID TAGGED USERS" + validTaggedUsers);
+        }
+        String editedText = text;
+        for (String user : validTaggedUsers) {
+          String userWithTag = "@" + user;
+          editedText = editedText.replace(userWithTag, "<span class='userMentioned'>" + userWithTag + "</span>");
+        }
+        return editedText;
+    }
+
+
+// TESTING FOR USERS
+     public static String styleUserTags(String text){
+     if(UserStore.getInstance().getUser(text) != null) {
+       return "<span id='userMentioned'>" + text + "</span>";
+     } else {
+       return "DOESN'T WORK";
+     }
+   }
+
+
+   public static void main(String[] args) {
+     System.out.println("here");
+     System.out.println(styleUserTags2("hi @Talia, @Edrei and @Kevin! How are you?"));
+     System.out.println(styleUserTags("@Talia"));
+  }
 }
