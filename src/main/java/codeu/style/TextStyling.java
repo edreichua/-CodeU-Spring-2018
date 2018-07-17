ce8b82f7d937e92933f3619213216bf4bc5c84f8
Talia Bahar <taliabahar@Talias-MacBook-Pro-2.local>
2018-07-16 22:43:55 -0700

    fixed spacing

--- src/main/java/codeu/style/TextStyling.java
+++ src/main/java/codeu/style/TextStyling.java
@@ -27,40 +27,39 @@ public class TextStyling {
     *                       decimal or hexadecimal html entity
     *  @return String with emojis to be ustilized in chat.jsp
     *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-   public static String emojifyText(String text){
-     String emojified = EmojiUtils.emojify(text);
-     return emojified;
-   }
-
+    public static String emojifyText(String text){
+      String emojified = EmojiUtils.emojify(text);
+      return emojified;
+    }
 
    /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     *  Method to style tagged users in chat differently
     *  @param  text         String representing user's original message
     *  @return String with a span wrapped around tagged usernames to assign it special styling properties
     *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-   public static String styleTaggedUsers(String text) {
-        List<String> taggedUsers = new ArrayList<>();
-        List<String> userNames = new ArrayList<>();
-        List<String> validTaggedUsers = new ArrayList<>();
-        String pattern = "@[A-Za-z0-9]*";
-        Pattern r = Pattern.compile(pattern);
-        Matcher m = r.matcher(text);
-        while (m.find( )) {
-          taggedUsers.add(m.group(0) );
+    public static String styleTaggedUsers(String text) {
+      List<String> taggedUsers = new ArrayList<>();
+      List<String> userNames = new ArrayList<>();
+      List<String> validTaggedUsers = new ArrayList<>();
+      String pattern = "@[A-Za-z0-9]*";
+      Pattern r = Pattern.compile(pattern);
+      Matcher m = r.matcher(text);
+      while (m.find( )) {
+        taggedUsers.add(m.group(0) );
+      }
+      for (String user : taggedUsers) {
+        userNames.add(user.substring(1,user.length()));
+      }
+      for (String userName : userNames) {
+        if(UserStore.getInstance().getUser(userName) != null) {
+          validTaggedUsers.add(userName);
         }
-        for (String user : taggedUsers) {
-          userNames.add(user.substring(1,user.length()));
-        }
-        for (String userName : userNames) {
-          if(UserStore.getInstance().getUser(userName) != null) {
-              validTaggedUsers.add(userName);
-          }
-        }
-        String editedText = text;
-        for (String user : validTaggedUsers) {
-          String userWithTag = "@" + user;
-          editedText = editedText.replace(userWithTag, "<span class='userMentioned'>" + userWithTag + "</span>");
-        }
-        return editedText;
+      }
+      String editedText = text;
+      for (String user : validTaggedUsers) {
+        String userWithTag = "@" + user;
+        editedText = editedText.replace(userWithTag, "<span class='userMentioned'>" + userWithTag + "</span>");
+      }
+      return editedText;
     }
-}
+  }
